b9f5228e9f1ef32dd0c3828905df14618f68537b
Mikhail Rudoy <mrudoy@gmail.com>
2012-02-14 00:38:50 -0500

    finished

--- 02_draw_line/draw.c
+++ 02_draw_line/draw.c
@@ -11,9 +11,11 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c)
   int dx, dy;
   int x, y, d;
 
+  d = 0;
+
   dx = x1-x0;
   dy = y1-y0;
-  /*
+  
   if(dx == 0)//vertical line
   {
     for(y = (dy > 0) ? y0 : y1; y <= y1 && y <= y0; y++)
@@ -61,11 +63,31 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c)
 
     if(dy < 0)//octant 8
     {
-      
+      for(x = x0, y = y0; x <= x1; x++)
+      {
+	plot(s, c, x, y);
+	if(d < 0)
+	{
+	  y--;
+	  d = d - dx;
+	}
+	x++;
+	d = d - dy;
+      }
     }
     else if(dx < 0)//octant 3
     {
-
+      for(y = y0, x = x0; y <= y1; y++)
+      {
+	plot(s, c, x, y);
+	if(d < 0)
+	{
+	  x--;
+	  d = d - dy;
+	}
+	y++;
+	d = d - dx;
+      }
     }
     else if(dx > dy)//octant 1
     {
@@ -83,9 +105,18 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c)
     }
     else//octant 2
     {
-
+      for(x = x0, y = y0; y <= y1; y++)
+      {
+	plot(s, c, x, y);
+	if(d > 0)
+	{
+	  x++;
+	  d = d + dy;
+	}
+	y++;
+	d = d - dx;
+      }
     } 
   }
-  */
 }
 
