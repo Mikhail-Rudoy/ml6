35a3e86cbe2a7f7a20661904de6b1346e0ff8354
Mikhail Rudoy <mrudoy@gmail.com>
2012-02-14 13:07:02 -0500

    first finished image

--- 02_draw_line/main.c
+++ 02_draw_line/main.c
@@ -13,33 +13,32 @@ int main()
 {
   screen s;
   color c;
-  int x = 150;
-  int y = 250;
-  int x2, y2;
+  double x = 20;
+  double y = 250;
+  double x2, y2;
   double i = 2;
 
   srand(time(0));
-  
+
   do
   {
     randomizeColor(&c);
     
-    //find x2, y2
-    x2 = (int)(250 + 100 * cos(i + atan2(y - 250, x - 250)));
-    y2 = (int)(250 + 100 * sin(i + atan2(y - 250, x - 250)));
-    
-    draw_line(x, y, x2, y2, s, c);
+    x2 = 250 + (cos(i) * (x - 250) - sin(i) * (y - 250));
+    y2 = 250 + (sin(i) * (x - 250) + cos(i) * (y - 250));
+
+    draw_line((int)x, (int)y, (int)x2, (int)y2, s, c);
     x = x2;
     y = y2;
   }
-  while(!(x == 150 && y == 250));
-
+  while(!(x < 20.1 && x > 19.9 && y < 250.1 && y > 249.9));
+  
   display(s); 
 }
 
 void randomizeColor(color* c)
 {
-  c->red = rand() % 255;
-  c->blue = rand() % 255;
-  c->green = rand() % 255;
+  c->red = rand() % 200 + 56;
+  c->blue = rand() % 200 + 56;
+  c->green = rand() % 200 + 56;
 }
