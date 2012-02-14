e2b7d83f72248b77baeb779b71e79b46358dc0e6
Mikhail Rudoy <mrudoy@gmail.com>
2012-02-14 11:51:50 -0500

    working on a drawing

--- 02_draw_line/main.c
+++ 02_draw_line/main.c
@@ -1,23 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #include "ml6.h"
 #include "display.h"
 #include "draw.h"
 
-int main() {
+void randomizeColor(color*);
 
+int main()
+{
   screen s;
   color c;
+  int x = 150;
+  int y = 250;
+  int x2, y2;
+  double i = 2;
 
-  c.red = 0;
-  c.green = 255;
-  c.blue = 255;
-
-  draw_line(0, 0, XRES, YRES-50, s, c);
-
-  display( s );
+  srand(time(0));
   
-  
-}  
+  do
+  {
+    randomizeColor(&c);
+    
+    //find x2, y2
+    x2 = (int)(250 + 100 * cos(i + atan2(y - 250, x - 250)));
+    y2 = (int)(250 + 100 * sin(i + atan2(y - 250, x - 250)));
+    
+    draw_line(x, y, x2, y2, s, c);
+    x = x2;
+    y = y2;
+  }
+  while(!(x == 150 && y == 250));
+
+  display(s); 
+}
+
+void randomizeColor(color* c)
+{
+  c->red = rand() % 255;
+  c->blue = rand() % 255;
+  c->green = rand() % 255;
+}
