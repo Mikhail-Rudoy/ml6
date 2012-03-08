4f4d0e11a8968c59961b2e0645ecfc071f9e067b
Mikhail Rudoy <mrudoy@gmail.com>
2012-03-08 10:54:05 -0500

    fixed direction

--- 02_draw_line/python/main.py
+++ 02_draw_line/python/main.py
@@ -4,5 +4,5 @@ sc = new_screen(600, 600)
 for x in range(75):
     for y in range(75):
         draw_line(sc, 300, 300, 4 + 8 * x, 4 + 8 * y, [255, 0, 0])
-draw_line(sc, 50, 50, 550, 50, [0, 255, 0])
+
 save_screen(sc, "pic.ppm")
