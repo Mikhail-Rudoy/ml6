75db56b29b466629be3d196cb27b94604454e101
Mikhail Rudoy <mrudoy@gmail.com>
2012-03-07 21:00:17 -0500

    testing graphics on other computer

---
+++ 01_PictureMaker/python/main.py
@@ -0,0 +1,18 @@
+from screen import *
+
+sc = new_screen(256, 256)
+for x in range(256):
+    for y in range(256):
+        sc[x][y][0] = x
+        sc[x][y][1] = y
+        sc[x][y][2] = 0
+        if x > 128:
+            sc[x][y][2] = sc[x][y][2] + x - 128
+        else:
+            sc[x][y][2] = sc[x][y][2] + 128 - x
+        if y > 128:
+            sc[x][y][2] = sc[x][y][2] + y - 128
+        else:
+            sc[x][y][2] = sc[x][y][2] + 128 - y
+
+save_screen(sc, "pic.ppm")
--- 01_PictureMaker/python/screen.py
+++ 01_PictureMaker/python/screen.py
@@ -8,4 +8,17 @@ def new_screen(w, h):
     return result
 
 def save_screen(screen, filename):
-    file = open(
+    FILE = open(filename, "w")
+    lines = ["P3"]
+    lines.append("" + str(len(screen)) + " " + str(len(screen[0])) + " 255")
+    for x in range(len(screen)):
+        next = ""
+        for y in range(len(screen[x])):
+            next = next + str(screen[x][y][0]) + " "
+            next = next + str(screen[x][y][1]) + " "
+            next = next + str(screen[x][y][2]) + " "
+        lines.append(next)
+    for i in range(len(lines)):
+        lines[i] = lines[i] + "\n"
+    FILE.writelines(lines)
+    FILE.close()
