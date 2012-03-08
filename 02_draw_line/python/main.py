688aa6446e506f93b70750acde3eb9475a1b299d
Mikhail Rudoy <mrudoy@gmail.com>
2012-03-07 22:31:24 -0500

    finished

--- 02_draw_line/python/main.py
+++ 02_draw_line/python/main.py
@@ -3,6 +3,6 @@ from screen import *
 sc = new_screen(600, 600)
 for x in range(75):
     for y in range(75):
-        draw_line(sc, 128, 128, 4 + 8 * x, 4 + 8 * y, [255, 0, 0])
+        draw_line(sc, 300, 300, 4 + 8 * x, 4 + 8 * y, [255, 0, 0])
 
 save_screen(sc, "pic.ppm")
