c043c68f153ce6aedab7fa36b1ed49a4396e3427
Mikhail Rudoy <mrudoy@gmail.com>
2012-03-14 19:09:48 -0400

    updating java 01 to include non version-tracked updates

--- 01_PictureMaker/java/main.java
+++ 01_PictureMaker/java/main.java
@@ -9,12 +9,12 @@ public class main
 	{
 	    for(int y = 0; y < 600; y++)
 	    {
-		int r = (int)Math.sqrt(Math.pow(x - 300, 2) + Math.pow(y - 300, 2));
-		int g = (int)(Math.sqrt(Math.pow(x - 100, 2) + Math.pow(y - 100, 2)) + Math.sqrt(Math.pow(x - 500, 2) + Math.pow(y - 500, 2)));
-		int b = (int)(Math.sqrt(Math.pow(x - 100, 2) + Math.pow(y - 500, 2)) + Math.sqrt(Math.pow(x - 500, 2) + Math.pow(y - 100, 2)));
+		int r = (int)((Math.sqrt(Math.pow(x - 300, 2) + Math.pow(y - 300, 2)))/1.655);
+		int g = 2 * 256-(int)((Math.sqrt(Math.pow(x - 100, 2) + Math.pow(y - 100, 2)) + Math.sqrt(Math.pow(x - 500, 2) + Math.pow(y - 500, 2)))/2.0);
+		int b = (int)((Math.sqrt(Math.pow(x - 100, 2) + Math.pow(y - 500, 2)) + Math.sqrt(Math.pow(x - 500, 2) + Math.pow(y - 100, 2)))/2.0);
 		s.setPixel(x, y, new Color(r%256, g%256, b%256));
 	    }
 	}
-	s.saveScreen("pic.ppm");
+	saveScreen("pic.ppm");
     }
 }
